962754bb9547fc5e65025eb7212ae9c4eecc0e25
Mathias Zechmeister <zechmeister@astro.physik.uni-goettingen.de>
2023-04-06 12:28:06 +0200

    chore: New format for NEONARVAL.

--- src/inst_NEONARVAL.py
+++ src/inst_NEONARVAL.py
@@ -7,7 +7,7 @@ obsloc = dict(lat=42.9333, lon= 0.1333, elevation=2869.4)
 pat = '*.fits'
 
 iomax = 39
-pmax =  7800 - 300
+pmax =  4208 - 300
 #oset = ':66'
 
 maskfile = 'telluric_mask_carm_short.dat'
@@ -41,9 +41,17 @@ def scan(self, s, pfits=True):
 def data(self, orders=None, pfits=True):
    hdulist = self.hdulist
    # read order data
-   f = hdulist[1].data['Intensity'].reshape(39,7800)[::-1][orders]
-   w_air = hdulist[1].data['Wavelength1'].reshape(39,7800)[::-1][orders]
-   e = hdulist[1].data['Error'].reshape(39,7800)[::-1][orders]
+   beam = '1'   # yet hardcoded
+   if self.filename.endswith('_st1.fits'):
+       #f = hdulist[1].data['Beam2'].reshape(39,7800)[::-1][orders]
+       #e = np.sqrt(np.abs(f) + 3**2)
+       f = hdulist[1].data['flux_'+beam].reshape(37,-1).astype('float64')[::-1][orders]
+       e = 1/hdulist[1].data['noise_'+beam].reshape(37,-1).astype('float64')[::-1][orders]
+   else:
+       f = hdulist[1].data['Intensity'].reshape(39,7800)[::-1][orders]
+       e = hdulist[1].data['Error'].reshape(39,7800)[::-1][orders]
+   #w_air = hdulist[1].data['Wavelength1'].reshape(39,7800)[::-1][orders]
+   w_air = hdulist[1].data['wavelength_'+beam].reshape(37,-1).astype('float64')[::-1][orders]
    w = airtovac(w_air)
 
    bpmap = np.isnan(f).astype(int)            # flag 1 for nan
