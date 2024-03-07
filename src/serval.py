9e48d3197bd710f2570dddf5ee4213ed539e9600
Mathias Zechmeister <zechmeister@astro.physik.uni-goettingen.de>
2024-03-07 17:36:55 +0100

    fix #65: Template masking bug.

--- src/serval.py
+++ src/serval.py
@@ -221,7 +221,7 @@ class Tpl:
               gplot(np.exp(wk), bk, ',', np.exp(wk), BK)
               pause()
 
-      self.msk = interp(wk, 1.*(BK>0))
+      self.msk = interp(self.wk, 1.*(BK>0))
 
    def __call__(self, w, der=0):
       return self.evalfunc(w, self.funcarg, der=der)
