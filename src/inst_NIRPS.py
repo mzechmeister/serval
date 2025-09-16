aac3c629aab8df2205611c3abbefc25edff4db08
Mathias Zechmeister <zechmeister@astro.physik.uni-goettingen.de>
2025-09-16 13:51:08 +0200

    chore: Default oset for NIRPS.

--- src/inst_NIRPS.py
+++ src/inst_NIRPS.py
@@ -14,7 +14,7 @@ maskfile = 'telluric_mask_carm_short.dat'
 
 pat = '*.tar *_S2D_SKYSUB_TELLSUB_A.fits'
 
-
+oset = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63]
 
 def scan(self, s, pfits=True, verb=False):
 
